8f45ece2cffa047775059549834315d788160b80
MOSCOW\Dmitrii_Smertin <Dmitrii_Smertin@epam.com>
2021-12-22 15:58:17 +0300

    create structure framework

---
+++ src/test/java/driver/DriverSingleton.java
@@ -0,0 +1,37 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverSingleton {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (null == driver) {
+//            switch (System.getProperty("browser")) {
+//                case "firefox": {
+//                    WebDriverManager.firefoxdriver().setup();
+//                    driver = new FirefoxDriver();
+//                    break;
+//                }
+//                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+//                }
+//             }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        }
+        return driver;
+    }
+
+    public static void closeBrowser() {
+        driver.quit();
+        driver = null;
+    }
+
+}
